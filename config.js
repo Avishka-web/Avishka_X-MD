f4737c883adefb1acbb2f5edd26e7e7acfa21a52
Avishka_X <avishkathuruwiman08@gmail.com>
2024-10-23 17:48:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || " Hear",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/2KVSNkv.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hi, I'm Avishka_X-MD.I'm Alive now.🔰.     ❤️This bot is now making it will finished one day ❤️"  
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi, I'm Avishka_X-MD.I'm Alive now.🔰.                  ❤️This bot is now making it will finished one day ❤️.                             🔰 This bot is powered by Avishka Thuruwiman_technology.🔰.                      ✅ NEW version will coming zoon💥"  
 };
