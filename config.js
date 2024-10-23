b5bbe8b530229e11a9a83704a5419182f304d61d
Avishka_X <avishkathuruwiman08@gmail.com>
2024-10-23 14:50:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Avishka-web/5/blob/5a31450e7e176d039f7ade189cb4c80a5c818c21/AK.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hi, I'm Avishka_X-MD.I'm Alive now.🔰"  
 };
